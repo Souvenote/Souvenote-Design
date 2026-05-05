19491d002ad2218cd05a2656d55b5fdea7d96330
Souvenote <cameron@souvenote.com>
2026-05-05 05:19:43 -0700

    Add client/src/data/mock-cards.ts

---
+++ client/src/data/mock-cards.ts
@@ -0,0 +1,20 @@
+import { CardTemplate } from '@/types/card';
+
+export const mockCards: CardTemplate[] = [
+  {
+    id: '1',
+    name: 'Classic Birthday Wishes',
+    occasion: 'Birthday',
+    physicalPreviewUrl: '',
+    digitalPreviewUrl: '',
+    basePriceCents: 1199,
+  },
+  {
+    id: '2',
+    name: 'Modern Anniversary Card',
+    occasion: 'Anniversary',
+    physicalPreviewUrl: '',
+    digitalPreviewUrl: '',
+    basePriceCents: 1399,
+  },
+];
